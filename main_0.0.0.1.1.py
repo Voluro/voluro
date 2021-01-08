5edbbb4695b433a19346e6e21d81d63e599617d0
rexillion <61246620+rexillion@users.noreply.github.com>
2021-01-08 12:45:03 +0200

    Create main_0.0.0.1.1.py

---
+++ main_0.0.0.1.1.py
@@ -0,0 +1,10 @@
+import turtle as t
+
+print("Welcome to Voluro 0.0.0.1")
+t.speed(0)
+t.hideturtle()
+t.color('red', 'yellow')
+t.color('black')
+style = ('Courier', 11)
+t.write('Welcome to Voluro 0.0.0.1.1 \n nothing to see here /:', font=style, align='center')
+t.done()
