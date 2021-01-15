e7ea6333d1537c2ef2a00b1f248de480a409f96f
rexillion <61246620+rexillion@users.noreply.github.com>
2021-01-15 12:39:44 +0200

    Added help changed pc name format and generally base variables.

--- no-gui 0.0.0.3.py
+++ no-gui 0.0.0.3.py
@@ -2,12 +2,12 @@ import time as ti
 print("Welcome to Voluro.py no-gui 0.0.0.2")
 loaded = False
 print("loading")
-basevars = [pcname = "Voluro"]
 while loaded != True:
-    def setbasevars(name, value)
-        basevars.name = value
+    pcname = "Voluro"
     loaded = True
 print("done")
 print("type help for help")
 while True:
-    input = input(basevars.pcname + ":")
+    inpt = input("@" + pcname + ":")
+    if inpt == 'help':
+        print("current commands: \nhelp: display all the commands and meanings")
