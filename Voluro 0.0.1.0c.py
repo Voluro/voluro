64a51d3f1461f5bf61a9a5b07de4dbf408173ec5
rexillion <61246620+rexillion@users.noreply.github.com>
2021-01-17 14:33:32 +0200

    Added auto app support and updated code fix

--- Voluro 0.0.1.0c.py
+++ Voluro 0.0.1.0c.py
@@ -6,12 +6,20 @@ except:
     temp = input()
     if temp == "y":
         file = open("savedsettings.py", "w")
-        file.write('''def load(name):
+        file.write('''
+def load(name):
     file = open(f'{name}.sdss', 'r')
     file = file.read()
     return file
+
+
 def save(name, val):
     file = open(f'{name}.sdss', 'w')
+    file.write(val)
+
+
+def append(name, val):
+    file = open(f'{name}.sdss', 'a')
     file.write(val)
 ''')
         exit("relaunch Voluro for the specified changes to take place")
@@ -31,6 +39,7 @@ while loaded != True:
         listapps: Lists all your apps.
         adap: adds the specified app.
         inap: opens the page for installing apps (Requires internet connection).
+        exec: executes code directly.
         '''
 
         global pos
@@ -38,7 +47,13 @@ while loaded != True:
         print("setting up apps...")
         global apps
         global running
-        apps = []
+        global allapps
+        try:
+            apps = savedsettings.load('apps')
+        except:
+            savedsettings.save('apps', '')
+            apps = savedsettings.load('apps')
+        allapps = apps.split(", ")
         # here go the rest of the configs
         print('loading previous settings')
         try:
@@ -46,6 +61,7 @@ while loaded != True:
         except:
             savedsettings.save("pcname", "Voluro")
             pcname1 = str(savedsettings.load("pcname"))
+        exec(f'import {apps}')
 
     reset()
     loaded = True
@@ -70,17 +86,27 @@ while True:
     elif inptlen == 2:
         if (inpt.split())[1] == '--pcname':
             import pcname
+
             pcname.pcname()
         elif (inpt.split())[0] == '-r':
             try:
                 exec(inpt.split()[1] + "." + inpt.split()[1] + "()")
             except:
-                print("error code 3 - invalid app")
+                try:
+                    exec('import' + " " + inpt.split()[1])
+                    exec(inpt.split()[1] + "." + inpt.split()[1] + "()")
+                except:
+                    print("error code 3 - invalid app")
         elif (inpt.split())[0] == 'adap':
             try:
                 exec('import' + " " + inpt.split()[1])
                 print('app ' + inpt.split()[1] + ' added')
-                apps.append(inpt.split()[1])
+                savedsettings.append('apps', ', ' + inpt.split()[1])
+                apps = savedsettings.load('apps')
+                try:
+                    allapps = apps.split(", ")
+                except:
+                    pass
             except:
                 print("error code 3 - invalid app")
         else:
@@ -92,7 +118,7 @@ while True:
         except:
             print("error code 4 - error in getting app")
     elif inpt == 'listapps':
-        for b in apps:
+        for b in allapps:
             print(b)
     else:
-        print("error code 1 - unknown command - invalid syntax")
+        Exception: print("error code 1 - unknown command - invalid syntax")
