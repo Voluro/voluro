68c7fe07a2b835357101cf0dbaa5288f6301369d
sergoum <77530549+sergoum@users.noreply.github.com>
2021-01-17 11:25:20 +0200

    Update clock.py
    it has to erase the clock, or else the terminal fills and it gets stuck. added s to stop. also made while true loop

--- clock.py
+++ clock.py
@@ -1,4 +1,8 @@
 import time as ti
+import keyboard
 def clock():
-    print("The time is:")
-    print(ti.asctime(ti.localtime()))
+    while True:
+        print("The time is:")
+        print(ti.asctime(ti.localtime()), end=\r, flush=True)
+        if keyboard.is_pressed('s'):
+            break
